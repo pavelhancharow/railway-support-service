9a28c3a210f92adbda904e1e7619a5c87b8788b8
Pavel <pl.hancharou@gmail.com>
2022-01-28 21:08:56 +0300

    fead: implement base styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 
-import App from './app/App';
+import './styles/index.scss';
+import { App } from './app/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));

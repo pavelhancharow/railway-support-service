3916a1bb71eaee063c1dc59217aa99bb0977659e
Pavel <pl.hancharou@gmail.com>
2022-02-01 17:36:44 +0300

    fix: implement redux toolkit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,19 @@
 import { FC, useEffect } from 'react';
 import { Header } from 'src/components/Header';
 import { Main } from 'src/components/Main';
-import { idb } from 'src/services/idb';
-import { routesDB } from 'src/db/routesDB';
+import { useAppDispatch, useAppSelector } from 'src/hooks/redux';
+import { fetchRoutes } from 'src/store/reducers/ActionCreator';
 
 export const App: FC = (): JSX.Element => {
+  const { isLoading, error } = useAppSelector((state) => state.routeReducer);
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
-    idb.initTable(routesDB);
-  }, []);
+    dispatch(fetchRoutes());
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (error.length) return <h1>${error}</h1>;
 
   return (
     <>
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import { App } from './app/App';
+import { setupStore } from './store/store';
 import { GlobalStyle } from './styles/globalStyle';
 
+const store = setupStore();
+
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     <GlobalStyle />
     <App />
-  </>,
+  </Provider>,
   document.getElementById('root')
 );

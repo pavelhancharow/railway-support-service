44a1b6cbadcd19123c4f521fe336194297595f3f
Pavel <pl.hancharou@gmail.com>
2022-02-05 20:13:01 +0300

    fix: implement Loader and MyError components

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,16 +2,30 @@ import { FC } from 'react';
 import { useAppSelector } from 'src/hooks/redux';
 import { MainBox } from 'src/shared/Main';
 import { Form } from './Form';
+import { Loader } from './Loader';
 import { MainInfo } from './MainInfo';
 import { Ticket } from './Ticket';
+import { MyError } from './UI/MyError';
 
 export const Main: FC = (): JSX.Element => {
-  const { formTicket } = useAppSelector(({ railwayReducer }) => railwayReducer);
+  const { railwayReducer, pageReducer } = useAppSelector((state) => state);
+  const { isLoading, error, formTicket } = railwayReducer;
+  const { adminError, adminIsLoading } = pageReducer;
+
+  const content = () => {
+    if (isLoading || adminIsLoading) return <Loader />;
+
+    if (error.length || adminError.length) {
+      return <MyError>${error || adminError}</MyError>;
+    }
+
+    return formTicket ? <Ticket /> : <Form />;
+  };
 
   return (
     <MainBox>
       <MainInfo />
-      {formTicket ? <Ticket /> : <Form />}
+      {content()}
     </MainBox>
   );
 };

737f8ad1b6adb35da7c50b983c2609c72b47f45f
Pavel <pl.hancharou@gmail.com>
2022-02-05 20:03:58 +0300

    fix: add page reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import railwayReducer from './reducers/RailwaySlice';
+import pageReducer from './reducers/PageSlice';
 
 const rootReducer = combineReducers({
   railwayReducer,
+  pageReducer,
 });
 
 export const setupStore = () => {

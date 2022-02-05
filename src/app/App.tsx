ab35cd9bdccbaba8c22f60da7eb0aeca50e9d504
Pavel <pl.hancharou@gmail.com>
2022-02-05 20:14:16 +0300

    fix: implement Login, Route components and getAdmin function

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,26 @@
 import { FC, useEffect } from 'react';
 import { Header } from 'src/components/Header';
+import { Login } from 'src/components/Login';
 import { Main } from 'src/components/Main';
-import { useAppDispatch, useAppSelector } from 'src/hooks/redux';
-import { fetchRailway } from 'src/store/reducers/ActionCreator';
+import { Route } from 'src/components/Route';
+import { useAppDispatch } from 'src/hooks/redux';
+import { fetchRailway } from 'src/store/actionCreators/RailwayCreator';
+import { getAdmin } from 'src/store/actionCreators/PageCreator';
 
 export const App: FC = (): JSX.Element => {
-  const { isLoading, error } = useAppSelector((state) => state.railwayReducer);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchRailway());
+    dispatch(getAdmin());
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  if (isLoading) return <h1>Loading...</h1>;
-  if (error.length) return <h1>${error}</h1>;
 
   return (
     <>
       <Header />
       <Main />
+      <Login />
+      <Route />
     </>
   );
 };

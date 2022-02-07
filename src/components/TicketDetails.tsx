65ca77c2ba22dd91e93c6cd9d3024098e6266005
Pavel <pl.hancharou@gmail.com>
2022-02-07 16:22:30 +0300

    feat: implement google maps api

--- src/components/TicketDetails.tsx
+++ src/components/TicketDetails.tsx
@@ -1,10 +1,60 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { TicketDetailsBox } from 'src/shared/TicketDetails';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { useAppDispatch, useAppSelector } from 'src/hooks/redux';
+import { setDuration } from 'src/store/actionCreators/RailwayCreator';
+
+const containerStyle = { width: '400px', height: '400px' };
+const center = { lat: 53.893009, lng: 27.567444 };
 
 export const TicketDetails: FC = (): JSX.Element => {
-  return (
+  const { ticket } = useAppSelector((state) => state.railwayReducer);
+  const dispatch = useAppDispatch();
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyBuJMyAuPqZATtIAnoPxOpQvgVcp6bzERc',
+    language: 'EN',
+  });
+
+  const onLoad = useCallback(
+    async function callback(map) {
+      const directionsService = new window.google.maps.DirectionsService();
+      const directionsDisplay = new window.google.maps.DirectionsRenderer();
+
+      directionsDisplay.setMap(map);
+
+      const request = {
+        origin: ticket.from,
+        destination: ticket.to,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+      };
+
+      await directionsService.route(request, (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          const duration = result!.routes[0].legs[0].duration!.text;
+          dispatch(setDuration(duration));
+          directionsDisplay.setDirections(result);
+        } else {
+          directionsDisplay.setDirections({ routes: [] });
+        }
+      });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [ticket]
+  );
+
+  return isLoaded ? (
     <TicketDetailsBox>
-      <li>Google API</li>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        onLoad={onLoad}
+      />
     </TicketDetailsBox>
+  ) : (
+    <TicketDetailsBox>Google Map is not available</TicketDetailsBox>
   );
 };

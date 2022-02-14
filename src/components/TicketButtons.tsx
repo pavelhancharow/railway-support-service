12adb3b2ef13cabb1ceebfd7902b6fb05995a763
Pavel <pl.hancharou@gmail.com>
2022-02-14 12:34:12 +0300

    fix: add button to home in train ticket

--- src/components/TicketButtons.tsx
+++ src/components/TicketButtons.tsx
@@ -2,13 +2,20 @@ import { FC } from 'react';
 import { TicketButtonsBox } from 'src/shared/TicketButtons';
 import { MyButton } from './UI/MyButton';
 import chevron from '../assets/svg/chevron.svg';
+import { useAppDispatch } from 'src/hooks/redux';
+import { toHomePage } from 'src/store/actionCreators/RailwayCreator';
 
 export const TicketButtons: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch();
+
   return (
     <TicketButtonsBox>
       <MyButton>
         <img src={chevron} alt="chevron" />
         Details
+      </MyButton>
+      <MyButton handleClick={() => dispatch(toHomePage())}>
+        Back To Home
       </MyButton>
       <MyButton>Reserve</MyButton>
     </TicketButtonsBox>
